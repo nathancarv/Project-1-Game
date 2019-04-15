84b3a838d17cb9120e55fbd0e4c8874bd354c9b2
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-04-15 13:14:12 -0400

    first commit

---
+++ starter-code/src/main.js
@@ -0,0 +1,81 @@
+var cards = [
+  { name: 'binoculars',      img: '<i class="fas fa-binoculars"></i>' },
+  { name: 'campground',      img: '<i class="fas fa-campground"></i>' },
+  { name: 'fire',            img: '<i class="fas fa-fire"></i>' },
+  { name: 'first-aid',       img: '<i class="fas fa-first-aid"></i>' },
+  { name: 'hiking',          img: '<i class="fas fa-hiking"></i>' },
+  { name: 'map',             img: '<i class="fas fa-map-marked"></i>' },
+  { name: 'mountain',        img: '<i class="fas fa-mountain"></i>' },
+  { name: 'tree',            img: '<i class="fas fa-tree"></i>' },
+  { name: 'binoculars',      img: '<i class="fas fa-binoculars"></i>' },
+  { name: 'campground',      img: '<i class="fas fa-campground"></i>' },
+  { name: 'fire',            img: '<i class="fas fa-fire"></i>' },
+  { name: 'first-aid',       img: '<i class="fas fa-first-aid"></i>' },
+  { name: 'hiking',          img: '<i class="fas fa-hiking"></i>' },
+  { name: 'map',             img: '<i class="fas fa-map-marked"></i>' },
+  { name: 'mountain',        img: '<i class="fas fa-mountain"></i>' },
+  { name: 'tree',            img: '<i class="fas fa-tree"></i>' },
+];
+
+var memoryGame = new MemoryGame(cards);
+
+//document.addEventListener("DOMContentLoaded", function(event) { 
+  var html = '';
+  memoryGame.cards.forEach(function (pic) {
+    html += '<div class="card" data-card-name="'+ pic.name +'">';
+    html += '  <div class="back" name= '+ pic.name +'>'+ pic.img +'</div>';
+    html += '  <div class="front"></div>'; /* style="background: url(img/'+ pic.img +') no-repeat"*/
+    html += '</div>';
+  });
+
+  // Add all the div's to the HTML
+  document.getElementById('memory_board').innerHTML = html;
+  // Bind the click event of each element to a function
+$('.back').on('click', function () {
+    if (!this.classList.contains('active')) {
+      memoryGame.pickedCards.push(this);
+      console.log('hi')
+      displayClickedCard(this);
+      if(memoryGame.pickedCards.length > 1) {
+        $('.front,.back').addClass('blocked');
+        var cards1 = memoryGame.pickedCards[0].getAttribute("name");
+        var cards2 = memoryGame.pickedCards[1].getAttribute("name");
+        window.c = memoryGame.pickedCards[0] 
+        console.log('here', cards1, cards2, '????')
+        if(memoryGame.checkIfPair(cards1, cards2)) { //true if correct else false
+          prepareNextTurn(); //got it right
+        } else {
+          turnBackCards(); //got it wrong 
+        }
+      }
+      printGameInfo();
+      if (memoryGame.isFinished()) { alert('You wooon!!!'); }
+    }
+});
+
+  // Bind the click event of each element to a function
+  function turnBackCards() {
+    setTimeout(function () {
+      memoryGame.pickedCards[0].style.background = '#456783';
+      memoryGame.pickedCards[1].style.background = '#456783';
+      memoryGame.pickedCards[0].classList.remove('active');
+      memoryGame.pickedCards[1].classList.remove('active');
+      prepareNextTurn();
+    }, 1000);
+  }
+  
+  function prepareNextTurn() {
+    memoryGame.pickedCards = [];
+    $('.front,.back').removeClass('blocked');
+  }
+  
+  function printGameInfo() {
+    document.getElementById('pairs_clicked').innerHTML = memoryGame.pairsClicked;
+    document.getElementById('pairs_guessed').innerHTML = memoryGame.pairsGuessed;
+  }
+  
+  function displayClickedCard(cards) {
+    console.log(cards)
+    cards.className += ' active';
+    // cards.style.background = 'url(img/' + cards.getAttribute('name') + ') no-repeat';
+  }
---
+++ starter-code/src/memory.js
@@ -0,0 +1,75 @@
+
+class MemoryGame {
+  constructor(cards){
+    this.cards = cards;
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.pickedCards = [];
+    this.cards = this.shuffleCards(cards);
+  }
+  shuffleCards() {
+    console.log("starting to shuffle cards", this.cards)
+
+  let copy = this.cards
+  let tempCard;
+  let indexCard;
+
+  for (let i = copy.length - 1; i > 0; i--) {
+    let randomCard = Math.floor(Math.random() * i); 
+    tempCard = copy[randomCard];
+    indexCard = copy[i];
+
+    copy[i] = tempCard;
+    copy[randomCard] = indexCard
+    
+  }
+
+  console.log("done shuffling cards ------- ", copy);
+  return copy;
+  }
+  checkIfPair(cards1, cards2) {
+
+      console.log('checkpair',this.pickedCards,cards1, cards2)
+      if (cards1 === cards2){
+        this.pairsGuessed++;
+      }
+      return cards1 === cards2;
+  }
+  isFinished() {
+    return this.pairsGuessed === 8;
+  }
+}
+
+
+// MemoryGame.prototype.shuffleCards = function(cards){
+//   console.log("starting to shuffle cards", cards)
+
+//   let copy = cards
+//   let shuffledDeck = [];
+//   let tempCard;
+//   let indexCard;
+
+//   for (let i = copy.length; i > 0; index--) {
+//     let randomCard = Math.floor(Math.random() * i); 
+//     tempCard = copy[randomCard];
+//     indexCard = copy[i];
+
+//     copy[i] = tempCard;
+//     copy[randomCard] = indexCard
+    
+//   }
+
+//   console.log("done shuffling cards ------- ", copy);
+//   return copy;
+
+
+//   // for(i = 0; i < copy.length; i++){
+//   //   let randomCard = Math.floor(Math.random() * cardArray.length); 
+//   //   let index = copy.indexOf(copy[i])
+//   //   shuffledDeck.push(copy[randomCard])
+//   //     // find the index of the randomCard pulled using indexof
+//   //     // remove (splice) the card from the copy array
+//   // }
+
+
+// }
